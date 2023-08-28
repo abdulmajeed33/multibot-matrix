63f2078b70c62ddabf1ec6bfa6d5ce4ee3e2392a
Abdul Majeed <abdulmajeedamm33@gmail.com>
2023-08-28 14:32:21 +0500

    hot fix

--- app/api/streaming/route.ts
+++ app/api/streaming/route.ts
@@ -1,3 +1,6 @@
+import { checkApiLimit, incrementApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
+import { auth } from "@clerk/nextjs";
 import { OpenAI } from "langchain";
 import { ConversationChain } from "langchain/chains";
 import { BufferMemory } from "langchain/memory";
@@ -9,11 +12,34 @@ let chain: ConversationChain | undefined;
 
 export async function POST(req: Request) {
   try {
+    const { userId } = auth();
     const body = await req.json();
     const { input, firstMsg } = body;
 
     if (!input) {
       return new NextResponse("Input is required", { status: 400 });
+    }
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!process.env.OPENAI_API_KEY) {
+      return new NextResponse("OpenAI API Key not configured.", {
+        status: 500,
+      });
+    }
+
+    const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
+
+    if (!freeTrial && !isPro) {
+      return new NextResponse(
+        "Free trial has expired. Please upgrade to pro.",
+        {
+          status: 403,
+        }
+      );
     }
 
     if (firstMsg) {
@@ -34,6 +60,9 @@ export async function POST(req: Request) {
 
     const response = await chain?.call({ input });
     console.log("response", response!);
+    if (!isPro) {
+      await incrementApiLimit();
+    }
     return new NextResponse(JSON.stringify({ output: response }), {
       status: 200,
     });
